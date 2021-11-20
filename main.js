74671e06203c5f6ffa331595499b998758e8b649
Shreya142 <83279107+Shreya142@users.noreply.github.com>
2021-11-20 20:10:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup()
+{
+    canvas=createCanvas(640, 480);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw()
+{
+  image(video, 0,0,640,480);
+}
